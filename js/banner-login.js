286fc09099f106d07ccba8e6c2ff0c422a4523c1
COMPUTO 09 <COMPUTO 09>
2017-04-21 13:56:19 -0500

    Dar funcionalida banner

--- js/banner-login.js
+++ js/banner-login.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
     $(".contenedor-carrucel__opciones").click(function() {
-        alert("si");
-        if ($(this).hasClass('.contenedor-carrucel__opcion-1')) {
-            alert("1");
-        } else if ($(this).hasClass('.contenedor-carrucel__opcion-2')) {
-            alert("2");
-        } else if ($(this).hasClass('.contenedor-carrucel__opcion-3')) {
-            alert("3");
-        } else if ($(this).hasClass('.contenedor-carrucel__opcion-4')) {
-            alert("4");
+        var rutaImagen = "";
+        if ($(this).hasClass('contenedor-carrucel__opcion-1')) {
+            rutaImagen = "img/imgBanner1.png";
+        } else if ($(this).hasClass('contenedor-carrucel__opcion-2')) {
+            rutaImagen = "img/imgBanner2.png";
+        } else if ($(this).hasClass('contenedor-carrucel__opcion-3')) {
+            rutaImagen = "img/imgBanner3.png";
+        } else if ($(this).hasClass('contenedor-carrucel__opcion-4')) {
+            rutaImagen = "img/imgBanner4.png";
         }
+        $(".contenedor-carrucel__img").animate({opacity:'0'}, 300, function() {
+            $(".contenedor-carrucel__img").attr("src", rutaImagen);
+        });        
+        $(".contenedor-carrucel__img").animate({opacity:'1'}, 300);
     })
 })
